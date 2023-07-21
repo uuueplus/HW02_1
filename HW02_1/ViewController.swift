c78599988b6e6df8f9f6a9df99c092ae0d75b364
Arthur Hung <uuueplus@gmail.com>
2023-07-22 06:20:13 +0800

    Initial Commit

---
+++ HW02_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW02_1
+//
+//  Created by Arthur Hung on 2023/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
